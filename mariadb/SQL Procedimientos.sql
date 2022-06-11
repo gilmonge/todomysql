5056e600573605f728c30b4d192323aa4588484c
Gilberth Monge <gilmonge1996@gmail.com>
2022-06-11 08:27:25 -0600

    DataBase Create

---
+++ mariadb/SQL Procedimientos.sql
@@ -0,0 +1,181 @@
+
+
+/* crear usuario */
+    DELIMITER //
+    CREATE PROCEDURE proc_user_create(
+        var_email varchar(80),
+        var_password varchar(50),
+        var_token varchar(15),
+
+        var_firstName varchar(50),
+        var_lastName varchar(50),
+        var_dateOfBirth date
+    )
+    BEGIN
+        INSERT INTO `tbl_user` SET 
+            `email` = var_email,
+            `password` = var_password,
+            `token` = var_token;
+
+        INSERT INTO `tbl_profile` SET
+            `firstName` = var_firstName,
+            `lastName` = var_lastName,
+            `dateOfBirth` = var_dateOfBirth,
+            `fk_user` = ( SELECT LAST_INSERT_ID() );
+    END;
+    //
+    DELIMITER ;
+
+    CALL proc_user_create(
+        'var_email',
+        'var_password',
+        'var_token',
+
+        'var_firstName',
+        'var_lastName',
+        '2022/06/08'
+    );
+/* crear usuario */
+
+/* Actualizar contraseña */
+    DELIMITER //
+    CREATE PROCEDURE proc_user_update_pass(
+        var_id INT(9),
+        var_password varchar(50),
+        var_token varchar(15)
+    )
+    BEGIN
+        UPDATE `tbl_user` SET 
+            `password`=var_password,
+            `token`=var_token
+        WHERE `id`=var_id;
+    END;
+    //
+    DELIMITER ;
+
+    CALL proc_user_update_pass(
+        1,
+        'nuevaPass',
+        'NuevoToken'
+    );
+/* Actualizar contraseña */
+
+/* Actualizar usuario */
+    DELIMITER //
+    CREATE PROCEDURE proc_user_update(
+        var_id INT(9),
+        var_firstName varchar(50),
+        var_lastName varchar(50),
+        var_dateOfBirth date
+    )
+    BEGIN
+        UPDATE `tbl_profile` SET 
+            `firstName` = var_firstName,
+            `lastName` = var_lastName,
+            `dateOfBirth` = var_dateOfBirth
+        WHERE `fk_user`=var_id;
+    END;
+    //
+    DELIMITER ;
+
+    CALL proc_user_update(
+        1,
+        'newName',
+        'NewLast',
+        '1996/02/05'
+    );
+/* Actualizar usuario */
+
+/* Crear categoria */
+    DELIMITER //
+    CREATE PROCEDURE proc_category_create(
+        var_id INT(9),
+        var_name varchar(50)
+    )
+    BEGIN
+        INSERT INTO `tbl_category_list` SET 
+            `name` = var_name,
+            `fk_user` = var_id;
+    END;
+    //
+    DELIMITER ;
+
+    CALL proc_category_create(
+        1, 
+        'Hogar'
+    );
+    CALL proc_category_create(
+        1, 
+        'Escuela'
+    );
+/* Crear categoria */
+
+/* Actualizar categoria */
+    DELIMITER //
+    CREATE PROCEDURE proc_category_update(
+        var_id INT(9),
+        var_name varchar(50)
+    )
+    BEGIN
+        UPDATE `tbl_category_list` SET 
+            `name` = var_name
+        WHERE `id` = var_id;
+    END;
+    //
+    DELIMITER ;
+
+    CALL proc_category_update(
+        1, 
+        'Hogar corregido 2'
+    );
+/* Actualizar categoria */
+
+/* Crear item */
+    DELIMITER //
+    CREATE PROCEDURE proc_item_create(
+        var_id INT(9),
+        var_title varchar(50),
+        var_detail varchar(100)
+    )
+    BEGIN
+        INSERT INTO `tbl_items` SET 
+            `title` = var_title,
+            `detail` = var_detail,
+            `status` = 0,
+            `fk_category_list` = var_id;
+    END;
+    //
+    DELIMITER ;
+
+    CALL proc_item_create(
+        1, 
+        'titulo',
+        'Contenido'
+    );
+/* Crear item */
+
+/* Crear item */
+    DELIMITER //
+    CREATE PROCEDURE proc_item_update(
+        var_id INT(9),
+        var_title varchar(50),
+        var_detail varchar(100),
+        var_status char(1)
+    )
+    BEGIN
+        UPDATE `tbl_items` SET 
+            `title` = var_title,
+            `detail` = var_detail,
+            `status` = var_status
+        WHERE `id` = var_id;
+    END;
+    //
+    DELIMITER ;
+
+    CALL proc_item_update(
+        1, 
+        'titulo edit',
+        'Contenido edit',
+        1
+    );
+/* Crear item */
---
+++ mariadb/dataBase.sql
@@ -0,0 +1,252 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: mariadbtodolist:3306
+-- Generation Time: Jun 09, 2022 at 04:57 AM
+-- Server version: 10.7.3-MariaDB-1:10.7.3+maria~focal
+-- PHP Version: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `todolist`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`%` PROCEDURE `proc_category_create` (`var_id` INT(9), `var_name` VARCHAR(50))   BEGIN
+        INSERT INTO `tbl_category_list` SET 
+            `name` = var_name,
+            `fk_user` = var_id;
+    END$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `proc_category_update` (`var_id` INT(9), `var_name` VARCHAR(50))   BEGIN
+        UPDATE `tbl_category_list` SET 
+            `name` = var_name
+        WHERE `id` = var_id;
+    END$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `proc_item_create` (`var_id` INT(9), `var_title` VARCHAR(50), `var_detail` VARCHAR(100))   BEGIN
+        INSERT INTO `tbl_items` SET 
+            `title` = var_title,
+            `detail` = var_detail,
+            `status` = 0,
+            `fk_category_list` = var_id;
+    END$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `proc_item_update` (`var_id` INT(9), `var_title` VARCHAR(50), `var_detail` VARCHAR(100), `var_status` CHAR(1))   BEGIN
+        UPDATE `tbl_items` SET 
+            `title` = var_title,
+            `detail` = var_detail,
+            `status` = var_status
+        WHERE `id` = var_id;
+    END$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `proc_user_create` (`var_email` VARCHAR(80), `var_password` VARCHAR(50), `var_token` VARCHAR(15), `var_firstName` VARCHAR(50), `var_lastName` VARCHAR(50), `var_dateOfBirth` DATE)   BEGIN
+    INSERT INTO `tbl_user` SET 
+        `email` = var_email,
+        `password` = var_password,
+        `token` = var_token;
+
+    INSERT INTO `tbl_profile` SET
+        `firstName` = var_firstName,
+        `lastName` = var_lastName,
+        `dateOfBirth` = var_dateOfBirth,
+        `fk_user` = ( SELECT LAST_INSERT_ID() );
+END$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `proc_user_update` (`var_id` INT(9), `var_firstName` VARCHAR(50), `var_lastName` VARCHAR(50), `var_dateOfBirth` DATE)   BEGIN
+        UPDATE `tbl_profile` SET 
+            `firstName` = var_firstName,
+            `lastName` = var_lastName,
+            `dateOfBirth` = var_dateOfBirth
+        WHERE `fk_user`=var_id;
+    END$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `proc_user_update_pass` (`var_id` INT(9), `var_password` VARCHAR(50), `var_token` VARCHAR(15))   BEGIN
+    UPDATE `tbl_user` SET 
+        `password`=var_password,
+        `token`=var_token
+    WHERE `id`=var_id;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_category_list`
+--
+
+CREATE TABLE `tbl_category_list` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `fk_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_category_list`
+--
+
+INSERT INTO `tbl_category_list` (`id`, `name`, `fk_user`) VALUES
+(1, 'Hogar corregido 2', 1),
+(2, 'Hogar corregido', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_items`
+--
+
+CREATE TABLE `tbl_items` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `detail` varchar(100) NOT NULL,
+  `status` char(1) NOT NULL DEFAULT '0',
+  `fk_category_list` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_items`
+--
+
+INSERT INTO `tbl_items` (`id`, `title`, `detail`, `status`, `fk_category_list`) VALUES
+(1, 'titulo edit', 'Contenido edit', '1', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_profile`
+--
+
+CREATE TABLE `tbl_profile` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `dateOfBirth` date NOT NULL,
+  `fk_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_profile`
+--
+
+INSERT INTO `tbl_profile` (`id`, `firstName`, `lastName`, `dateOfBirth`, `fk_user`) VALUES
+(1, 'newName', 'NewLast', '1996-02-05', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `token` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `email`, `password`, `token`) VALUES
+(1, 'var_email', 'nuevaPass', 'NuevoToken');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_category_list`
+--
+ALTER TABLE `tbl_category_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tbl_category_list_tbl_user1_idx` (`fk_user`);
+
+--
+-- Indexes for table `tbl_items`
+--
+ALTER TABLE `tbl_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tbl_items_tbl_category_list1_idx` (`fk_category_list`);
+
+--
+-- Indexes for table `tbl_profile`
+--
+ALTER TABLE `tbl_profile`
+  ADD PRIMARY KEY (`id`,`fk_user`),
+  ADD KEY `fk_tbl_profile_tbl_user1_idx` (`fk_user`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_category_list`
+--
+ALTER TABLE `tbl_category_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_items`
+--
+ALTER TABLE `tbl_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_profile`
+--
+ALTER TABLE `tbl_profile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_category_list`
+--
+ALTER TABLE `tbl_category_list`
+  ADD CONSTRAINT `fk_tbl_category_list_tbl_user1` FOREIGN KEY (`fk_user`) REFERENCES `tbl_user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `tbl_items`
+--
+ALTER TABLE `tbl_items`
+  ADD CONSTRAINT `fk_tbl_items_tbl_category_list1` FOREIGN KEY (`fk_category_list`) REFERENCES `tbl_category_list` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `tbl_profile`
+--
+ALTER TABLE `tbl_profile`
+  ADD CONSTRAINT `fk_tbl_profile_tbl_user1` FOREIGN KEY (`fk_user`) REFERENCES `tbl_user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
